1898dcd3d75db6634e94f8d8b44820a066e70bb3
Rahul Mekala <rahul.mekala@zapcg.com>
2025-04-09 14:20:03 +0530

    Adding Controller prototype

---
+++ RMS_Dashboard/Controllers/EmployeesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RMS_Dashboard.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+    }
+}
