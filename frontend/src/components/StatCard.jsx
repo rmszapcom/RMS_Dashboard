59928a24de87148a77973aed09550897a8d6cdfc
AmithaPai <amitha.pai@zapcg.com>
2025-04-16 12:13:06 +0530

    Added Dashboard Cards

---
+++ frontend/src/components/StatCard.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./StatCard.css";
+import { Paper, Typography, Box } from "@mui/material";
+
+const StatCard = ({ title, value, icon }) => {
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        p: 2,
+        height: "100%",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        minWidth: 200,
+        borderRadius: 3,
+        backgroundColor: "#000080",
+        color: "white",
+        transition: "transform 0.3s ease-in-out",
+        "&:hover": {
+          transform: "scale(1.05)",
+          boxShadow: "0px 8px 24px rgba(0, 0, 0, 0.2)",
+        },
+      }}
+    >
+      <Box sx={{ flex: 1 }}>
+        <Typography
+          variant="subtitle1"
+          color="text.secondary"
+          sx={{ fontWeight: 600, mb: 1 }}
+        >
+          {title}
+        </Typography>
+        <Typography variant="h6" sx={{ fontWeight: 500, fontSize: "1.1rem" }}>
+          {value}
+        </Typography>
+      </Box>
+      {icon && (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: "50%", // Circular shape
+            // backgroundColor: "white",
+            boxShadow: `0px 4px 12px ${icon.props.color}`,
+            padding: 1,
+          }}
+        >
+          {icon}
+        </Box>
+      )}
+    </Paper>
+  );
+};
+
+export default StatCard;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
