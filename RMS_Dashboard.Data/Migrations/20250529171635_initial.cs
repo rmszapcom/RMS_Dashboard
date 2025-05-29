b99bfa4f092af663f6d9c1b35c6d995c1eef3f85
soumya.bawage <soumya.bawage@accelya.com>
2025-05-29 23:41:29 +0530

    updated employee Model and DB according to recent changes required

--- RMS_Dashboard.Data/Migrations/20250529171635_initial.cs
+++ RMS_Dashboard.Data/Migrations/20250529171635_initial.cs
@@ -7,7 +7,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace RMS_Dashboard.Data.Migrations
 {
     /// <inheritdoc />
-    public partial class initialcreate : Migration
+    public partial class initial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -25,11 +25,7 @@ namespace RMS_Dashboard.Data.Migrations
                     RelevantExpPrimary = table.Column<string>(type: "text", nullable: false),
                     SecondarySkills = table.Column<string>(type: "text", nullable: false),
                     RelevantExpSecondary = table.Column<string>(type: "text", nullable: false),
-                    Remarks = table.Column<string>(type: "text", nullable: false),
-                    Skill = table.Column<string>(type: "text", nullable: false),
                     ProjectName = table.Column<string>(type: "text", nullable: false),
-                    Department = table.Column<string>(type: "text", nullable: true),
-                    Location = table.Column<string>(type: "text", nullable: false),
                     WorkLocation = table.Column<string>(type: "text", nullable: false),
                     ReportingManager = table.Column<string>(type: "text", nullable: false),
                     OverAllExperience = table.Column<string>(type: "text", nullable: false),
@@ -39,16 +35,12 @@ namespace RMS_Dashboard.Data.Migrations
                     DateOfJoining = table.Column<string>(type: "text", nullable: false),
                     WorkMode = table.Column<string>(type: "text", nullable: false),
                     BenchStatus = table.Column<string>(type: "text", nullable: false),
-                    ExitStatus = table.Column<string>(type: "text", nullable: false),
-                    TrainingPlanAssigned = table.Column<string>(type: "text", nullable: false),
-                    TrainingCompletionStatus = table.Column<string>(type: "text", nullable: false),
-                    PerformanceRating = table.Column<string>(type: "text", nullable: false),
+                    IsEngaged = table.Column<string>(type: "text", nullable: true),
+                    IsAllocated = table.Column<string>(type: "text", nullable: true),
                     ExitDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
                     TotalExpYears = table.Column<decimal>(type: "numeric", nullable: true),
                     BenchStartDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
-                    FitmentScore = table.Column<int>(type: "integer", nullable: true),
-                    ExpectedRollOffDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
-                    Salary = table.Column<string>(type: "text", nullable: false)
+                    ExpectedRollOffDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
                 },
                 constraints: table =>
                 {
@@ -74,51 +66,6 @@ namespace RMS_Dashboard.Data.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Project", x => x.ProjectID);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "BenchExitDetail",
-                columns: table => new
-                {
-                    BenchExitID = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    EmpID = table.Column<string>(type: "text", nullable: false),
-                    ExitReason = table.Column<string>(type: "text", nullable: false),
-                    BenchCategory = table.Column<string>(type: "text", nullable: false),
-                    UpdatedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_BenchExitDetail", x => x.BenchExitID);
-                    table.ForeignKey(
-                        name: "FK_BenchExitDetail_Employees_EmpID",
-                        column: x => x.EmpID,
-                        principalTable: "Employees",
-                        principalColumn: "EmployeeID",
-                        onDelete: ReferentialAction.Cascade);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "EmployeeSkill",
-                columns: table => new
-                {
-                    SkillID = table.Column<int>(type: "integer", nullable: false)
-                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    EmpID = table.Column<string>(type: "text", nullable: false),
-                    TechStack = table.Column<string>(type: "text", nullable: false),
-                    Skill = table.Column<string>(type: "text", nullable: false),
-                    SkillDescription = table.Column<string>(type: "text", nullable: false),
-                    PastInitiative = table.Column<string>(type: "text", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_EmployeeSkill", x => x.SkillID);
-                    table.ForeignKey(
-                        name: "FK_EmployeeSkill_Employees_EmpID",
-                        column: x => x.EmpID,
-                        principalTable: "Employees",
-                        principalColumn: "EmployeeID",
-                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateTable(
@@ -158,28 +105,17 @@ namespace RMS_Dashboard.Data.Migrations
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
                     EmployeeName = table.Column<string>(type: "text", nullable: false),
                     ProjectName = table.Column<string>(type: "text", nullable: false),
-                    EmployeeID = table.Column<string>(type: "text", nullable: true),
                     ProjectID = table.Column<int>(type: "integer", nullable: true)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_ResourceAllocations", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_ResourceAllocations_Employees_EmployeeID",
-                        column: x => x.EmployeeID,
-                        principalTable: "Employees",
-                        principalColumn: "EmployeeID");
-                    table.ForeignKey(
                         name: "FK_ResourceAllocations_Project_ProjectID",
                         column: x => x.ProjectID,
                         principalTable: "Project",
                         principalColumn: "ProjectID");
                 });
-
-            migrationBuilder.CreateIndex(
-                name: "IX_BenchExitDetail_EmpID",
-                table: "BenchExitDetail",
-                column: "EmpID");
 
             migrationBuilder.CreateIndex(
                 name: "IX_EmployeeEngagementPlan_EmpID",
@@ -192,16 +128,6 @@ namespace RMS_Dashboard.Data.Migrations
                 column: "ProjectID");
 
             migrationBuilder.CreateIndex(
-                name: "IX_EmployeeSkill_EmpID",
-                table: "EmployeeSkill",
-                column: "EmpID");
-
-            migrationBuilder.CreateIndex(
-                name: "IX_ResourceAllocations_EmployeeID",
-                table: "ResourceAllocations",
-                column: "EmployeeID");
-
-            migrationBuilder.CreateIndex(
                 name: "IX_ResourceAllocations_ProjectID",
                 table: "ResourceAllocations",
                 column: "ProjectID");
@@ -211,13 +137,7 @@ namespace RMS_Dashboard.Data.Migrations
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "BenchExitDetail");
-
-            migrationBuilder.DropTable(
                 name: "EmployeeEngagementPlan");
-
-            migrationBuilder.DropTable(
-                name: "EmployeeSkill");
 
             migrationBuilder.DropTable(
                 name: "ResourceAllocations");
